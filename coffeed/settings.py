a9e606e38553bffb034e467b27f14602c2ed3719
makenoise <tankbragdon@gmail.com>
2015-05-03 16:22:44 -0500

    Troubleshooting Static Issue

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(MAIN_DIR,'static'),
+    os.path.join(BASE_DIR,'static'),
     )
 
 STATIC_ROOT = 'staticfiles'
--- coffeed/urls.py
+++ coffeed/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 import core.views as coreviews
 
 urlpatterns = [
@@ -9,4 +11,4 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',coreviews.SplashView.as_view()),
-]
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
